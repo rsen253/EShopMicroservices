d5463113f527a71ac4faf4db878ff45aa7bdad1e
Rahul Sen <rsen253@gmail.com>
2024-08-10 12:33:34 +0530

    Registered the carter and mediator in program cs completed basic flow of adding product using command handler and mapster route

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container (AddServiceMethod)
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the https request pipeline (UseMethod)
-
+app.MapCarter();
 app.Run();
