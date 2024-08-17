042623494d66599238ef5d6e8bfc404224a257ed
Rahul Sen <rsen253@gmail.com>
2024-08-17 17:00:01 +0530

    Added CRUD operation endpoint in the discount api

---
+++ src/Services/Discount/Discount.Grps/Data/DiscountContext.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grps.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grps.Data;
+
+public class DiscountContext : DbContext
+{
+    public DiscountContext(DbContextOptions option)
+        : base(option)
+    {
+    }
+    public DbSet<Coupon> Coupons { get; set; } = default;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+            );
+    }
+}
---
+++ src/Services/Discount/Discount.Grps/Data/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grps.Data;
+
+public static class Extentions
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        // migrate the database automatically 
+        dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
