a9fd0a5b3801895bbd4cdfa40a28cc12237f8a33
Rahul Sen <rsen253@gmail.com>
2024-08-21 21:21:03 +0530

    Added custom type to ignore primitive obsation

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,18 +1,18 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class OrderItem : Entity<Guid>
+public class OrderItem : Entity<OrderId>
 {
-    internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
     {
-        Id = orderId;
+        Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
         ProductId = productId;
         Quantity = quantity;
         Price = price;
     }
 
-    public Guid OrderId { get; private set; }
-    public Guid ProductId { get; private set; }
+    public OrderId OrderId { get; private set; }
+    public ProductId ProductId { get; private set; }
     public int Quantity { get; private set; }
     public decimal Price { get; private set; }
 }
