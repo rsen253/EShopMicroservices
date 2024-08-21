1643193bb5bddb8ab154c52d70a8bb7e0d40d60d
Rahul Sen <rsen253@gmail.com>
2024-08-21 20:12:47 +0530

    Added basic domain model, value object and enums

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<Guid>
+{
+    internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+    {
+        Id = orderId;
+        OrderId = orderId;
+        ProductId = productId;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public Guid OrderId { get; private set; }
+    public Guid ProductId { get; private set; }
+    public int Quantity { get; private set; }
+    public decimal Price { get; private set; }
+}
