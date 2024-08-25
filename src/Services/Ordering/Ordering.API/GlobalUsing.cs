5297b5433b965a547ee32b8a1280927ac9de6874
Rahul Sen <rsen253@gmail.com>
2024-08-25 14:55:05 +0530

    Added ordering micro services different endpoints , custom exception & health check middleware

---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Carter;
+global using Ordering.Application.Dtos;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Orders.Commands.CreateOrder;
+global using Ordering.Application.Orders.Commands.UpdateOrder;
+global using Ordering.Application.Orders.Commands.DeleteOrder;
+global using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+global using Ordering.Application.Orders.Queries.GetOrdersByName;
+global using BuildingBlocks.Pagination;
+global using Ordering.Application.Orders.Queries.GetOrders;
